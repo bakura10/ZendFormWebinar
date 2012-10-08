94e43ab6076cc6a85b3a58a8cbd48c8b617395fc
Michaël Gallego <mic.gallego@gmail.com>
2012-10-09 00:01:09 +0200

    Ajout des entités

---
+++ module/Application/src/Application/Entity/Band.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Application\Entity;
+
+class Band
+{
+    /** @var string */
+    public $name;
+
+    /** @var string */
+    public $websiteUrl;
+}
---
+++ module/Application/src/Application/Entity/Concert.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Application\Entity;
+
+class Concert
+{
+    /** @var Band */
+    public $band;
+
+    /** @var int */
+    public $availableSeats;
+}
